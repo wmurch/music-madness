971951202d511c2457c6ed50735558c25eec866a
Will Murchison <wmurch@hotmail.com>
2019-06-13 19:32:45 -0400

    added new image and corrected navigation menu

--- src/components/HomeData.jsx
+++ src/components/HomeData.jsx
@@ -4,13 +4,11 @@ class HomeData extends Component {
   render() {
     return (
       <>
-        <main>
-          <div>
-            <h1 className="homeText">
-              Welcome to Will's wonderful world of music
-            </h1>
-          </div>
-        </main>
+        <div>
+          <h1 className="homeText">
+            Welcome to Will's wonderful world of music
+          </h1>
+        </div>
       </>
     )
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,9 @@ class Home extends Component {
   render() {
     return (
       <>
-        <HomeData />
+        <div>
+          <HomeData />
+        </div>
       </>
     )
   }
